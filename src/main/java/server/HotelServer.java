a7af19a6faf6052bc87785fc8f360af2e21c5a83
Olga Karpenko <okarpenko@usfca.edu>
2023-10-31 14:35:14 -0700

    Exercise

---
+++ src/main/java/server/HotelServer.java
@@ -0,0 +1,15 @@
+package server;
+
+import hotelapp.HotelReviewData;
+import hotelapp.Review;
+import org.eclipse.jetty.servlet.ServletHandler;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletHolder;
+import  org.eclipse.jetty.servlet.ServletContextHandler;
+public class HotelServer {
+	public static final int PORT = 8080;
+
+	public static void main(String[] args)   {
+		// FILL IN CODE
+	}
+}
---
+++ src/main/java/server/HotelServlet.java
@@ -0,0 +1,16 @@
+package server;
+
+import hotelapp.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class HotelServlet extends HttpServlet {
+	// FILL IN CODE
+
+}
